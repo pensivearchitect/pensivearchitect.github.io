47cab3dc964a72c3c10d7b8416d78a1b1cbc14be
Joshua Bell <josh@joshuabell.io>
2018-08-27 21:51:16 -0700

    cleaned up scss and fixed under_construction style

--- src/components/under_construction.js
+++ src/components/under_construction.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const UnderConstruction = () => (
-    <div className="has-text-centered container under-construction">
-      <h1 className="h1--title-border has-text-weight-normal">Under Construction</h1>
-    </div>
+  <div className="has-text-centered container under-construction">
+    <h1 className="title h1--title-border">Under Construction</h1>
+  </div>
 );
 
 export default UnderConstruction;
